fb8de625ce3317811a17a9336d5bf4edeba53ae0
rmit-s3529120-thomas-higgins <s3529120@student.rmit.edu.au>
2018-03-31 16:45:02 +1100

    Fixed file read for binary type

--- src/dbquery/dbquery.java
+++ src/dbquery/dbquery.java
@@ -1,37 +1,59 @@
 package dbquery;
 
-import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class dbquery {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		searchHeap(args[0],args[1]);
 
 	}
 	
-	public static void searchHeap(String searchword,int pagesize){
+	public static void searchHeap(String searchword,String pagesize){
 		String line="",recline;
-		String filename="heapfile"+pagesize+".dat";
+		String filename="heapfile."+pagesize+".dat";
 		String[] record;
 		ArrayList<String> matches=new ArrayList<String>();
 		
-		int pages=1;
+		int pages=1,records=0;
 		//Read in heap file
-		try (BufferedReader br = new BufferedReader(new FileReader(filename))){
+		//try (BufferedReader br = new BufferedReader(new FileReader(filename))){
+		try(DataInputStream in =  new DataInputStream(new FileInputStream(filename))){
 			//Read in each line
-			while((line=br.readLine())!=null) {
+			boolean remaining=true,sizeIndicator=true;
+			while(remaining) {
+				records++;
+				//Read records per page
+				if(sizeIndicator){
+					try{
+						int s=in.readInt();
+						line=String.valueOf(s);
+						sizeIndicator=false;
+					}catch(EOFException e){
+						remaining=false;
+						break;
+					}
+				}
+				try{
+					line=in.readUTF();
+				}catch(EOFException e){
+					remaining=false;
+				}
+				
 				//If new page marker encountered
 				if(line.compareTo("<")==0){
+					System.out.println("Page "+pages+" - "+records+" records:");
+					records=0;
 					//Print matched record finds for page
 					for(String rec:matches){
-						System.out.println("Page "+pages+":");
 						System.out.println(rec);
 					}
-					System.out.print("\n");
+					
 					
 					//Increment page number
 					pages++;
@@ -39,22 +61,25 @@ public class dbquery {
 					//Reset matches
 					matches=new ArrayList<String>();
 					
+					//Set indicator for reading records per page
+					sizeIndicator=true;
+					
 					//Continue reading from next page
 					continue;
 				}
 				
 				//Split record line and generate print string
-				record=line.split(">");
-				if(record[0].compareToIgnoreCase(searchword)==0){
+				record=line.split(">",-1);
+				if(record[0].toLowerCase().contains(searchword.toLowerCase())){
 					recline="RECORD MATCH "+matches.size()+1+":\n";
-					recline.concat("Business name: "+record[0]+"\n");
-					recline.concat("Status: "+record[1]+"\n"); 
-					recline.concat("Registration date: "+record[2]+"\n");
-					recline.concat("Cancellation date: "+record[3]+"\n"); 
-					recline.concat("Renewal date: "+record[4]+"\n"); 
-					recline.concat("Former state number: "+record[5]+"\n"); 
-					recline.concat("Former state of registration: "+record[6]+"\n"); 
-					recline.concat("ABN: "+record[7]+"\n");
+					recline=recline.concat("    -Business name: "+record[0]+"\n");
+					recline=recline.concat("    -Status: "+record[1]+"\n"); 
+					recline=recline.concat("    -Registration date: "+record[2]+"\n");
+					recline=recline.concat("    -Cancellation date: "+record[3]+"\n"); 
+					recline=recline.concat("    -Renewal date: "+record[4]+"\n"); 
+					recline=recline.concat("    -Former state number: "+record[5]+"\n"); 
+					recline=recline.concat("    -Former state of registration: "+record[6]+"\n");
+					recline=recline.concat("    -ABN: "+record[7]+"\n");
 					matches.add(recline);
 					
 				}
@@ -62,11 +87,14 @@ public class dbquery {
 				
 			}
 			}catch(FileNotFoundException e){
-				
+				System.err.println(filename+" not found.");
 			}catch(IOException e){
-				
+				System.err.println("Heapfile read failed.");
+				e.printStackTrace();
 			}
+		
 			
 	}
+	
 
 }
